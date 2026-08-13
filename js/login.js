216015a6575086e33e9190f12b4f052e92ed811d
NikTak777 <nikkondrakhin@gmail.com>
2026-08-13 22:12:20 +0300

    Переписана логика входа в аккаунт. Добавлен запрос с backend

--- js/login.js
+++ js/login.js
@@ -3,24 +3,39 @@ const emailInput = document.getElementById('email-input')
 const passwordInput = document.getElementById('password-input')
 const backBtn = document.getElementById('back-btn')
 
-const userData = {
-    email: 'admin@admin.com',
-    password: 'password'
-}
-
-loginForm.addEventListener('submit', function(event) {
+loginForm.addEventListener('submit', async function(event) {
     event.preventDefault()
 
-    if (emailInput.value !== userData.email) {
-        alert('Такого аккаунта не существует!')
-        emailInput.value = ''
-        passwordInput.value = ''
-    } else if (passwordInput.value !== userData.password) {
-        alert('Неверный пароль!')
-        passwordInput.value = ''
-    } else {
-        alert('Вход успешно выполнен!')
-        window.location.href = 'index.html'
+    const payload = {
+        email: emailInput.value,
+        password: passwordInput.value
+    }
+    try {
+        const response = await fetch('http://127.0.0.1:8000/v1/auth/login',
+        {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            }, 
+            body: JSON.stringify(payload),
+            credentials: 'include'
+        }
+        )
+
+        if (response.ok) {
+            const data = await response.json()
+
+            localStorage.setItem('access_token', data.access_token);
+
+            alert('Вход успешно осуществлён! Добро пожаловать!');
+
+            window.location.href = 'index.html'; 
+        } else {
+            const errorData = await response.json();
+            alert(errorData.detail || 'Ошибка входа. Попробуйте ещё раз');
+        }
+    } catch (error) {
+        console.error('Ошибка входа:', error)
     }
 })
 
