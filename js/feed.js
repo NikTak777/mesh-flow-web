79fb5084d46320d1447fd4b4bcb5a737036a493f
NikTak777 <nikkondrakhin@gmail.com>
2026-08-13 15:25:21 +0300

    Добавлен post запрос к backend для добавления нового поста

--- js/feed.js
+++ js/feed.js
@@ -2,7 +2,7 @@ const newPost = document.getElementById('create-post-form')
 const titlePost = document.getElementById('new-post-title')
 const descriptionPost = document.getElementById('new-post-description')
 
-newPost.addEventListener('submit', function(event) {
+newPost.addEventListener('submit', async function(event) {
     event.preventDefault();
 
     const post = {
@@ -14,11 +14,21 @@ newPost.addEventListener('submit', function(event) {
         comments: 0
     }
 
-    alert(`
-    Новый пост:
-    ${titlePost.value}
-    ${descriptionPost.value}
-    `)
+    console.log("Новый пост с фронта: ", post)
+
+    const response = await fetch('http://127.0.0.1:8000/v1/post', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json' 
+            },
+            body: JSON.stringify({
+                title: titlePost.value,
+                description: descriptionPost.value
+            })
+        });
+
+    const result = await response.json();
+    console.log("Ответ сервера: ", result);
 
     window.location.href = 'index.html'
 
