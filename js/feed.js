fd33783df59eacbdd92cacf5c814e855c628accf
NikTak777 <nikkondrakhin@gmail.com>
2026-08-15 14:08:03 +0300

    Новый пост начал выкладываться от имени аккаунта пользователя

--- js/feed.js
+++ js/feed.js
@@ -5,30 +5,33 @@ const descriptionPost = document.getElementById('new-post-description')
 newPost.addEventListener('submit', async function(event) {
     event.preventDefault();
 
-    const post = {
-        channelName: "Обо всём и ни о чем",
-        timeAgo: "Только что",
-        title: titlePost.value,
-        text: descriptionPost.value,
-        likes: 0,
-        comments: 0
-    }
+    const token = localStorage.getItem('access_token')
 
-    console.log("Новый пост с фронта: ", post)
+    let response = await fetch(
+        'http://127.0.0.1:8000/v1/auth/me',
+        {
+            method: 'GET',
+            headers: {
+                'Authorization': `Bearer ${token}`
+            }
+        }
+    )
 
-    const response = await fetch('http://127.0.0.1:8000/v1/post', {
+    if (response.ok) {
+        const userData = await response.json()
+
+        response = await fetch('http://127.0.0.1:8000/v1/post', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json' 
             },
             body: JSON.stringify({
+                channelName: userData['login'],
                 title: titlePost.value,
                 description: descriptionPost.value
             })
         });
-
-    const result = await response.json();
-    console.log("Ответ сервера: ", result);
+    }
 
     window.location.href = 'index.html'
 
